a3b0c79a1bde48fb877e49bf88c22fab54fde28b
Viktorija778 <viktorijajucikaite0@gmail.com>
2026-06-18 09:19:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("KRATC bot paleistas")
+import requests
+
+url = "https://www.kratc.lt/produktai/mainu-stotele-imk/klaipeda/"
+
+r = requests.get(url, timeout=30)
+
+print(r.status_code)
+
+if r.status_code == 200:
+    print("KRATC puslapis pasiekiamas")
+else:
+    print("Klaida")
